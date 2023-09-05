72bda67c80ad05bfc4f90b87fa3b8c0aa8e833a6
Sevynidd <Sevynidd02@protonmail.com>
2023-09-05 20:00:03 +0200

    Scaffold mit BottomBar und SplashScreen hinzugefügt

--- app/src/main/java/agb/loehne/blutspende_app/MainActivity.kt
+++ app/src/main/java/agb/loehne/blutspende_app/MainActivity.kt
@@ -1,53 +1,62 @@
 package agb.loehne.blutspende_app
 
+import agb.loehne.blutspende_app.ui.theme.Blutspende_AppTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import agb.loehne.blutspende_app.ui.theme.Blutspende_AppTheme
-import android.os.Build
-import android.view.Window
-import android.view.WindowManager
-import androidx.annotation.RequiresApi
-import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Setzen eines Splashscreens
+        installSplashScreen()
+        //Fullscreen App
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             Blutspende_AppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NavigationBarBottom()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Blutspende_AppTheme {
-        Greeting("Android")
+fun NavigationBarBottom() {
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+                Text("Bottom Bar")
+            }
+        },
+        contentWindowInsets = WindowInsets(20.dp, 30.dp, 20.dp, 0.dp)
+    ) { paddingValues ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)) {
+            Text("Content")
+        }
     }
 }
--- app/src/main/java/agb/loehne/blutspende_app/ui/theme/Theme.kt
+++ app/src/main/java/agb/loehne/blutspende_app/ui/theme/Theme.kt
@@ -1,12 +1,16 @@
 package agb.loehne.blutspende_app.ui.theme
 
-import android.os.Build
-import androidx.annotation.RequiresApi
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 
 private val LightColors = lightColorScheme(
@@ -79,11 +83,20 @@ fun Blutspende_AppTheme(
   useDarkTheme: Boolean = isSystemInDarkTheme(),
   content: @Composable() () -> Unit
 ) {
-  val colors = if (useDarkTheme) {
-    DarkColors
-  } else {
-    LightColors
+  val colors = when {
+    useDarkTheme -> DarkColors
+      else -> LightColors
   }
+    val view = LocalView.current
+    if (!view.isInEditMode){
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = Color.Transparent.toArgb()
+            window.navigationBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !useDarkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !useDarkTheme
+        }
+    }
 
   MaterialTheme(
     colorScheme = colors,
